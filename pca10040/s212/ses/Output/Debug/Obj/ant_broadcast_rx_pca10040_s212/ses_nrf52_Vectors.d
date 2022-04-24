7f93fef2c0bb44fe999a2c96e3368d6a2f5ace75
Andrea <andrea32.rossi@mail.polimi.it>
2022-04-24 10:35:31 +0200

    Progetto ANT

---
+++ pca10040/s212/ses/Output/Debug/Obj/ant_broadcast_rx_pca10040_s212/ses_nrf52_Vectors.d
@@ -0,0 +1,2 @@
+Output/Debug/Obj/ant_broadcast_rx_pca10040_s212/ses_nrf52_Vectors.o: \
+ C:\firmware\ lucchesini\nRF5_SDK_14.2.0_17b948a\components\toolchain\ses\ses_nrf52_Vectors.s
---
+++ pca10040/s212/ses/Output/Debug/Obj/ant_broadcast_rx_pca10040_s212/thumb_crt0.d
@@ -0,0 +1,2 @@
+Output/Debug/Obj/ant_broadcast_rx_pca10040_s212/thumb_crt0.o: \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.70a/source/thumb_crt0.s
